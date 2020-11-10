516795b7f254753f11a7d4bda8041d0ef382e2f4
faizruzain <faiz.faiz8383@gmail.com>
2020-11-10 15:57:04 +0700

    now we are using app.route methode

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ const bodyParser = require('body-parser');
 // ########################## MongoDB ##########################
 // getting-started.js
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/wikiDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost/wikiDB', {useNewUrlParser: true,useUnifiedTopology: true});
 
 //a connection to database
 const db = mongoose.connection;
@@ -33,44 +30,73 @@ const Articles = mongoose.model('Article', articlesSchema);
 app.set('view engine', 'ejs');
 
 //tell our app to use body-parser
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 //tell express serever to use static file called "public" or whatever
 app.use(express.static('public'));
 
-app.get('/articles', (req, res) => {
+// ############################ /articles ############################
+app.route('/articles')
+
+.get((req, res) => {
 
   Articles.find({}, (err, docs) => {
-    if(!err) {
+    if (!err) {
       res.send(docs);
     } else {
       res.send(err);
     }
   });
 
-});
+})
 
-app.post('/articles', (req, res) => {
+.post((req, res) => {
 
-  const newArticle = new Articles ({
+  const newArticle = new Articles({
     title: req.body.title,
     content: req.body.content
   });
 
   newArticle.save((err) => {
     if (!err) {
-      console.log('title: '+req.body.title+' has been saved');
-      console.log('content: '+req.body.content+' has been saved');
-      res.redirect('/articles');
+      res.send('Saved!');
     } else {
       res.send(err);
     }
   });
 
-});
+})
 
+.delete((req, res) => {
+
+  Articles.deleteMany({}, (err) => {
+    if (!err) {
+      res.send('Deleted!');
+    } else {
+      console.log(err);
+    }
+  });
+
+});
+// ############################ /articles ############################
+
+// ############################ /articles/:params ############################
+app.route('/articles/:articleTitle')
+
+.get((req, res) => {
+
+  const articleTitle = req.params.articleTitle;
+
+  Articles.findOne({title: articleTitle}, (err, doc) => {
+    if (!err) {
+      res.send(doc);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+// ############################ /articles/:params ############################
 
 
 
